95e8cfb01527ca20336024e62ee1295a20fee442
Kurilenko Aleksandr <ninznak@gmail.com>
2022-10-02 12:11:56 +0300

    добавлены функции view и urls

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Главная страница')
 
-def group_posts(request):
-    return HttpResponse('Посты в группе')
+def group_posts(request, slug):
+    return HttpResponse(f'Посты в группе{slug}')
+
+def name(request):
+    return HttpResponse('Привет мидвед')
 
